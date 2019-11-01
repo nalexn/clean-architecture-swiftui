45f18ff41820f269755552663b5208f52f78fdae
Alexey Naumov <alexey.naumov@chimpchange.me>
2019-11-01 18:25:49 +0300

    Remove scene configuration from app delegate

--- CountriesSwiftUI/System/AppDelegate.swift
+++ CountriesSwiftUI/System/AppDelegate.swift
@@ -15,10 +15,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
 }
