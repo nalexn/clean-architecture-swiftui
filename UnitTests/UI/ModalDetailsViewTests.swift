d70a9a8bcb315931624eb679db42e036652b923c
Alexey Naumov <alexey.naumov@chimpchange.me>
2019-11-01 15:55:50 +0300

    Add tests for modal details

---
+++ UnitTests/UI/ModalDetailsViewTests.swift
@@ -0,0 +1,26 @@
+//
+//  ModalDetailsViewTests.swift
+//  UnitTests
+//
+//  Created by Alexey Naumov on 01.11.2019.
+//  Copyright © 2019 Alexey Naumov. All rights reserved.
+//
+
+import XCTest
+import SwiftUI
+@testable import CountriesSwiftUI
+
+class ModalDetailsViewTests: XCTestCase {
+
+    func test_modalDetails() {
+        let exp = XCTestExpectation(description: "onAppear")
+        let isDisplayed = State<Bool>(initialValue: true)
+        let sut = ModalDetailsView(country: Country.mockedData[0], isDisplayed: isDisplayed.projectedValue)
+            .asyncOnAppear {
+                RootViewInjection.unmount()
+                exp.fulfill()
+            }
+        RootViewInjection.mount(view: sut, environment: RootViewModifier(appState: AppState()))
+        wait(for: [exp], timeout: 1)
+    }
+}
