79a073c965f3856e518930a4089a3733436594ae
Alexey Naumov <a.naumov91@gmail.com>
2020-01-09 18:03:11 +0300

    Remove custom view injection

--- CountriesSwiftUI/UI/Screens/ContentView.swift
+++ CountriesSwiftUI/UI/Screens/ContentView.swift
@@ -11,60 +11,17 @@ import Combine
 
 struct ContentView: View {
     
-    @State private var otherViews: [StackItem] = ContentView.otherViews.value
     private let container: DIContainer
-    internal var isRunningTests: Bool = ProcessInfo.processInfo.isRunningTests
     
     init(container: DIContainer) {
         self.container = container
     }
     
     var body: some View {
-        ZStack {
-            if isRunningTests {
-                EmptyView()
-                ForEach(otherViews) { $0.view }
-                    .onReceive(ContentView.otherViews) { self.otherViews = $0 }
-            } else {
-                realContent.inject(container)
-            }
-        }
-    }
-    
-    private var realContent: some View {
         CountriesList()
+            .inject(container)
     }
 }
-
-// MARK: - Adding Views to the hierarchy in tests
-
-private extension ContentView {
-    struct StackItem: Identifiable {
-        let id: String
-        let view: AnyView
-    }
-    static let otherViews: CurrentValueSubject<[StackItem], Never> = .init([])
-}
-
-#if DEBUG
-extension ContentView {
-    static func mount<V>(view: V, appState: AppState,
-                         interactors: DIContainer.Interactors,
-                         viewId: String = #function) where V: View {
-        let preparedView = view.inject(appState, interactors)
-        let item = StackItem(id: viewId, view: AnyView(preparedView))
-        otherViews.value += [item]
-    }
-    
-    static func unmount(viewId: String = #function) {
-        var views = otherViews.value
-        if let index = views.firstIndex(where: { $0.id == viewId }) {
-            views.remove(at: index)
-        }
-        otherViews.value = views
-    }
-}
-#endif
 
 // MARK: - Preview
 
