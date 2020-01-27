ae3ed113875cb52383e4d2a2807787068e4fc7b8
Alexey Naumov <a.naumov91@gmail.com>
2020-01-27 22:32:50 +0600

    Parse error for internet connection outage

--- CountriesSwiftUI/Utilities/Helpers.swift
+++ CountriesSwiftUI/Utilities/Helpers.swift
@@ -56,8 +56,19 @@ extension Publisher {
     
     func extractUnderlyingError() -> Publishers.MapError<Self, Failure> {
         mapError {
-            (($0 as NSError).userInfo[NSUnderlyingErrorKey] as? Failure) ?? $0
+            ($0.underlyingError as? Failure) ?? $0
         }
+    }
+}
+
+private extension Error {
+    var underlyingError: Error? {
+        let nsError = self as NSError
+        if nsError.domain == NSURLErrorDomain && nsError.code == -1009 {
+            // "The Internet connection appears to be offline."
+            return self
+        }
+        return nsError.userInfo[NSUnderlyingErrorKey] as? Error
     }
 }
 
