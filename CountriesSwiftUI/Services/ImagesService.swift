06001a36c6bb7ee777897734057eed959fe6600d
Shcherbinin Andrey <ascherbinin@live.ru>
2020-05-16 15:56:21 +0500

    Add simple caching on NSCache

--- CountriesSwiftUI/Services/ImagesService.swift
+++ CountriesSwiftUI/Services/ImagesService.swift
@@ -14,25 +14,43 @@ protocol ImagesService {
     func load(image: LoadableSubject<UIImage>, url: URL?)
 }
 
+struct ImageCache {
+    private let cache = NSCache<NSURL, UIImage>()
+
+    subscript(_ key: URL) -> UIImage? {
+        get { cache.object(forKey: key as NSURL) }
+        set { newValue == nil ? cache.removeObject(forKey: key as NSURL) : cache.setObject(newValue!, forKey: key as NSURL) }
+    }
+}
+
 struct RealImagesService: ImagesService {
-    
+
     let webRepository: ImageWebRepository
-    
+    var imageCache: ImageCache
+
     init(webRepository: ImageWebRepository) {
         self.webRepository = webRepository
+        self.imageCache = ImageCache()
     }
-    
+
     func load(image: LoadableSubject<UIImage>, url: URL?) {
         guard let url = url else {
             image.wrappedValue = .notRequested; return
         }
         let cancelBag = CancelBag()
-        image.wrappedValue = .isLoading(last: image.wrappedValue.value, cancelBag: cancelBag)
-        webRepository.load(imageURL: url, width: 300)
-            .sinkToLoadable {
-                image.wrappedValue = $0
+        if let caсhedImage = imageCache[url] {
+            image.wrappedValue = .loaded(caсhedImage)
+        }
+        else {
+            image.wrappedValue = .isLoading(last: image.wrappedValue.value, cancelBag: cancelBag)
+            var cache = self.imageCache
+            webRepository.load(imageURL: url, width: 300)
+                .sinkToLoadable {
+                    image.wrappedValue = $0
+                    cache[url] = $0.value
             }
             .store(in: cancelBag)
+        }
     }
 }
 
--- CountriesSwiftUI/System/AppDelegate.swift
+++ CountriesSwiftUI/System/AppDelegate.swift
@@ -21,6 +21,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions
         launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        URLCache.shared.removeAllCachedResponses()
         return true
     }
     
