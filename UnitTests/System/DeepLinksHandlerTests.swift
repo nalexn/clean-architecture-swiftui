be2367d2ee0c26286c2f518468d4bf3927911499
Alexey Naumov <alexey@naumov.tech>
2020-08-28 16:45:54 +0300

    A workaround for a navigation state bug in SwiftUI

--- UnitTests/System/DeepLinksHandlerTests.swift
+++ UnitTests/System/DeepLinksHandlerTests.swift
@@ -19,7 +19,7 @@ class DeepLinksHandlerTests: XCTestCase {
         XCTAssertEqual(container.appState.value, AppState())
     }
     
-    func test_openingFlag() {
+    func test_openingDeeplinkFromDefaultRouting() {
         let interactors: DIContainer.Interactors = .mocked()
         let initialState = AppState()
         let container = DIContainer(appState: initialState, interactors: interactors)
@@ -32,5 +32,29 @@ class DeepLinksHandlerTests: XCTestCase {
         expectedState.routing.countryDetails.detailsSheet = true
         interactors.verify()
         XCTAssertEqual(container.appState.value, expectedState)
+    }
+    
+    func test_openingDeeplinkFromNonDefaultRouting() {
+        let interactors: DIContainer.Interactors = .mocked()
+        var initialState = AppState()
+        initialState.routing.countriesList.countryDetails = "FRA"
+        initialState.routing.countryDetails.detailsSheet = true
+        let container = DIContainer(appState: initialState, interactors: interactors)
+        let sut = RealDeepLinksHandler(container: container)
+        sut.open(deepLink: .showCountryFlag(alpha3Code: "ITA"))
+        
+        let resettedState = AppState()
+        var finalState = AppState()
+        finalState.routing.countriesList.countryDetails = "ITA"
+        finalState.routing.countryDetails.detailsSheet = true
+        
+        XCTAssertEqual(container.appState.value, resettedState)
+        let exp = XCTestExpectation(description: #function)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            interactors.verify()
+            XCTAssertEqual(container.appState.value, finalState)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 2.5)
     }
 }
